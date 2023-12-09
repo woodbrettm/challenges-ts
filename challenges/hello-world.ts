e19a62bd84e4135bbb2fcf9f001544fc318168af
Brett Wood <brett@woodbrettm.com>
2023-12-09 04:46:31 +0000

    [ADD] type-challenges utils package

--- challenges/hello-world.ts
+++ challenges/hello-world.ts
@@ -2,5 +2,7 @@
   Challenge: https://github.com/type-challenges/type-challenges/blob/main/questions/00013-warm-hello-world/README.md
 */
 
+import { Expect, Equal } from '@type-challenges/utils';
+
 type HelloWorld = string;
 type Test = Expect<Equal<HelloWorld, string>>;
