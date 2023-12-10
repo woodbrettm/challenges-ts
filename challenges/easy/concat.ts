dbfadde0e2007bdb4b750d56c9926f1eebdde937
Brett Wood <brett@woodbrettm.com>
2023-12-10 03:15:33 +0000

    [ADD] Concat Challenge

---
+++ challenges/easy/concat.ts
@@ -0,0 +1,21 @@
+/*
+https://github.com/type-challenges/type-challenges/blob/main/questions/00533-easy-concat/README.md
+*/
+
+import { Expect, Equal } from '@type-challenges/utils';
+
+type TuplePrimitives = ReadonlyArray<string | number | boolean>;
+type Concat<T extends TuplePrimitives, K extends TuplePrimitives> = [...T, ...K];
+
+type Result = Concat<[1], [2]>;
+type Test = Expect<Equal<Result, [1, 2]>>;
+
+/*
+
+TODO:
+
+This meets the requirements of the challenge, however
+a: How to possibly handle unlimited generic args? Maybe generic arg should be an array, containing arrays to combine.
+b How to handle nested arrays?
+
+*/
