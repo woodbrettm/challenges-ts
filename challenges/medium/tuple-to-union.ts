db2d9210454aca5fc108c9440c9e9ecd127feafa
Brett Wood <brett@woodbrettm.com>
2023-12-19 21:11:30 +0000

    [ADD] Tuple to Union

---
+++ challenges/medium/tuple-to-union.ts
@@ -0,0 +1,12 @@
+/*
+https://github.com/type-challenges/type-challenges/blob/main/questions/00010-medium-tuple-to-union/README.md
+*/
+
+import { Expect, Equal } from '@type-challenges/utils';
+
+type Arr = ['1', '2', '3', ['4', '5', '6', ['7', '8']], ['9', '10']];
+
+type TupleToUnion<T> = T extends Array<infer U> ? TupleToUnion<U> : T;
+
+type Res = TupleToUnion<Arr>;
+type Test = Expect<Equal<Res, '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10'>>;
