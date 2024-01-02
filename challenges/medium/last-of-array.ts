9f96263c43e08f6fbbb4f6eada084d6ded04e822
Brett Wood <brett@woodbrettm.com>
2024-01-02 13:01:27 +0000

    [ADD] Last of Array

---
+++ challenges/medium/last-of-array.ts
@@ -0,0 +1,13 @@
+/*
+https://github.com/type-challenges/type-challenges/blob/main/questions/00015-medium-last/README.md
+*/
+
+import { Expect, Equal } from '@type-challenges/utils';
+
+type Last<T> = T extends [...Array<unknown>, infer U] ? U : never;
+
+type Tail1 = Last<['a', 'b', 'c']>; // expected to be 'c'
+type Tail2 = Last<[3, 2, 1]>; // expected to be 1
+
+type Test1 = Expect<Equal<Tail1, 'c'>>;
+type Test2 = Expect<Equal<Tail2, 1>>;
